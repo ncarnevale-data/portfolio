8fc6883bf22f65eec040d4bbb3369861ff969ebc
Nicolas Carnevale <ncarnevaledata@gmail.com>
2024-07-02 16:07:12 -0300

    Update NavBar menu for desktop, allowing scroll

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,30 +1,51 @@
 const NavBar = () => {
+  const scrollTo = (id: string) => {
+    const to = document.getElementById(id);
+    const navbarHeight = document.getElementById("navbar")!.offsetHeight;
+    if (to) {
+      const scrollTo = to.offsetTop - navbarHeight;
+      window.scrollTo({ top: scrollTo, behavior: "smooth" });
+    }
+  };
+
   return (
-    <header className="sticky top-0 z-10 md:mb-8">
+    <header className="sticky top-0 z-10 md:mb-8" id="navbar">
       <div className="flex items-center justify-around bg-brand-green p-4">
         <h3 className="cursor-pointer text-2xl lg:text-3xl">
-          <a href="#">NCarnevale</a>
+          <a href="/">NCarnevale</a>
         </h3>
         <ul className="hidden lg:flex lg:w-auto lg:items-center lg:space-x-3">
           <li>
-            <a href="#" className="text-sm">
+            <button
+              onClick={() => scrollTo("about")}
+              className="text-sm transition-colors duration-300 hover:text-brand-orange"
+            >
               SOBRE MÍ
-            </a>
+            </button>
           </li>
           <li>
-            <a href="#" className="text-sm">
+            <button
+              onClick={() => scrollTo("skills")}
+              className="text-sm transition-colors duration-300 hover:text-brand-orange"
+            >
               HABILIDADES
-            </a>
+            </button>
           </li>
           <li>
-            <a href="#" className="text-sm">
+            <button
+              onClick={() => scrollTo("projects")}
+              className="text-sm transition-colors duration-300 hover:text-brand-orange"
+            >
               PROYECTOS
-            </a>
+            </button>
           </li>
           <li>
-            <a href="#" className="text-sm">
+            <button
+              onClick={() => scrollTo("contact")}
+              className="text-sm transition-colors duration-300 hover:text-brand-orange"
+            >
               CONTACTO
-            </a>
+            </button>
           </li>
         </ul>
         <div className="">Socials/ColorModeSwitch</div>
