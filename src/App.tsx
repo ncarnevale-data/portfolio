894ec4f83bc1278e32a27c71ef58b045d95f9e78
Nicolas Carnevale <ncarnevaledata@gmail.com>
2024-07-02 15:56:26 -0300

    Scroll to the top when the component mounts

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
@@ -6,6 +7,11 @@ import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 
 function App() {
+  useEffect(() => {
+    // Scroll to the top when the component mounts
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className=" bg-brand-darkblue text-brand-white">
       <NavBar />
